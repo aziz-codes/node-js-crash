9c380bd5432b80c3ff5f190fd69ffddf730f28de
aziz-codes <azizisoffline@gmail.com>
2022-11-17 22:56:37 +0500

    reading file and using path module in node js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const {add, subtract, multiply, divide} = require('./math');
-
-console.log(add(2,2));
-console.log(subtract(2,2));
-console.log(multiply(2,2));
-console.log(divide(2,2));
+const fs = require("fs");
+const path = require("path");
+fs.readFile(
+  path.join(__dirname, "files", "starter.txt"),
+  "utf8",
+  (err, data) => {
+    if (err) {
+      console.log("There was an error ", err.message);
+    }
+    console.log(data);
+  }
+);
