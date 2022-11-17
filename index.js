4b13b241ee79be486a5386bd098caa4a30b4d519
aziz-codes <azizisoffline@gmail.com>
2022-11-17 23:06:17 +0500

    Write file in node js

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ fs.readFile(
     console.log(data);
   }
 );
+console.log(__filename);
+
+//writing file
+
+fs.writeFile(
+  path.join(__dirname, "files", "reply.txt"),
+  "This is how you can reaf to some files",
+  (err) => {
+    if (err) {
+      console.log("There was an error ", err.message);
+    }
+    console.log("data");
+  }
+);
